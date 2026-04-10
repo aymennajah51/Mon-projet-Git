3d3a1ee16365247b5b0c5dd9ecb0326cbe7d370f
Aymen Najah <aymennajah51@gmail.com>
2026-04-10 12:06:03 +0100

    fix: introduce bug for demonstration

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ function multiplication(a, b) {
     return a * b;
 }
 
+function addition(a, b) {
+    return a - b; // BUG
+}
+
 console.log("Addition:", addition(5, 3));
 console.log("Multiplication:", multiplication(5, 3));
