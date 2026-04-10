620a376b880a68e4e1b8a9f74faf7f60642762b6
Aymen Najah <aymennajah51@gmail.com>
2026-04-10 12:07:57 +0100

    fix: correct addition bug

--- script.js
+++ script.js
@@ -6,9 +6,5 @@ function multiplication(a, b) {
     return a * b;
 }
 
-function addition(a, b) {
-    return a - b; // BUG
-}
-
 console.log("Addition:", addition(5, 3));
 console.log("Multiplication:", multiplication(5, 3));
